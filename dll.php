9f489bfe73caf422cd40cc382814fd2bc6419168
Olatunbosun Egberinde <bosunski@gmail.com>
2019-03-06 08:27:34 +0100

    Add insertAtFirst

--- dll.php
+++ dll.php
@@ -18,5 +18,22 @@ class DoublyLinkedList
     private $lastNode = null;
     private $totalNode = null;
 
+    public function insertAtFirst(string $data = null)
+    {
+        $newNode = new ListNode($data);
 
+        if ($this->firstNode === null) {
+            $this->firstNode = &$newNode;
+            $this->lastNode = &$newNode;
+        } else {
+            $currentFirstNode = $this->firstNode;
+            $this->firstNode = &$newNode;
+            $newNode->next = $currentFirstNode;
+            $currentFirstNode->prev = $newNode;
+        }
+
+        $this->totalNode++;
+
+        return true;
+    }
 }
