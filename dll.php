3071a7975e67caff3ff3783c324d20fae30e6ccd
Olatunbosun Egberinde <bosunski@gmail.com>
2019-03-06 08:03:47 +0100

    Started doubly linked list

--- dll.php
+++ dll.php
@@ -1,1 +1,22 @@
 <?php
+
+class ListNode
+{
+    public $data = null;
+    public $next = null;
+    public $prev = null;
+
+    public function __construct(string $data = null)
+    {
+        $this->data = $data;
+    }
+}
+
+class DoublyLinkedList
+{
+    private $firstNode = null;
+    private $lastNode = null;
+    private $totalNode = null;
+
+
+}
