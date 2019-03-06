b6c10dde332c0ba0478d82afd0f6175c7b35f6c3
Olatunbosun Egberinde <bosunski@gmail.com>
2019-03-06 14:54:46 +0100

    Add  SplPriorityQueue.php

---
+++ SplPriorityQueue.php
@@ -0,0 +1,28 @@
+<?php
+class MyPQ extends SplPriorityQueue
+{
+    public function compare($priority1, $priority2)
+    {
+        return $priority1 <=> $priority2;
+    }
+}
+
+$agents = new MyPQ();
+
+$agents->insert("Fred", 1);
+$agents->insert("John", 2);
+$agents->insert("Keith", 3);
+$agents->insert("Adiyan", 4);
+$agents->insert("Mikhael", 2);
+
+// Mode of extraction
+$agents->setExtractFlags(MyPQ::EXTR_BOTH);
+
+// Go to TOP
+$agents->top();
+
+while ($agents->valid()) {
+    $current = $agents->current();
+    echo $current['data'], PHP_EOL;
+    $agents->next();
+}
